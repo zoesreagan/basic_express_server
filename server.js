18eebc7641b07c9377a6df943914b5514cd4b41a
Zoe <zoereagan@gmail.com>
2018-04-09 14:51:58 -0500

    made a show route

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/fruits', (req, res) => {
   res.send(fruits);
 })
 
+//SHOW route --- show all the info about one partiuclar fruit
+app.get('/fruits/:id', (req, res) => {
+  const index = req.params.id
+  res.send(fruits[index]);
+});
+
 app.listen(3000, () => {
   console.log("server is listening on Port 3000");
 });
