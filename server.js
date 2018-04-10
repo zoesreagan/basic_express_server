a59094a8fb25bfa6bc6843b273724baebbcf0d31
Zoe <zoereagan@gmail.com>
2018-04-10 12:04:51 -0500

    added body-parser middleware; wrote a post route to add data

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require ('express') //syntax for module (no . or /)
 const app = express();
+const bodyParser = require('body-parser')
+
 //our data
 //note the syntax to put in file, you need relative path--must start w/ "./"
 const fruits = require('./models/fruits.js')
+
+//MIDDLEWARE
+//must go above routes you are intercepting
+//you app.use MIDDLEWARE
+//next will continue process
+app.use((req, res, next) => {
+  console.log("I am middleware and will be run for all routes");
+  next();
+})
+//we are using the body-parser MIDDLEWARE
+//it is a module that will let us see the form data in our post requests
+app.use(bodyParser.urlencoded({extended: false}))
 
 //route (URL you can go to/address)
 //INDEX route-- this will list all the routes
@@ -40,7 +54,21 @@ app.get('/fruits/:id', (req, res) => {
 //post/fruits
 //no id needed, because this is a new fruit
 app.post('/fruits', (req, res) => {
-  res.send("you hit the post route!")
+  console.log(req.body);
+//add a new object to our fruits array
+//if req.body.readyToEat is on
+  const newFruit = {
+      name: req.body.name,
+      color: req.body.color,
+      readyToEat: req.body.readyToEat == 'on' ? true : false
+  }
+  // if(req.body.readyToEat == "on") {
+  //     newFruit.readyToEat = true;
+  // } else newFruit.readyToEat = false;
+
+  fruits.push(newFruit);
+  res.redirect('/fruits')
+
 });
 
 
