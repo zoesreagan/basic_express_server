10c8b1661b90fed3ecd05d2393d95f299a04cf8c
Zoe <zoereagan@gmail.com>
2018-04-11 11:32:51 -0500

    created controller directory and fruitsController file

---
+++ controllers/FruitController.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const router = express.Router();
+
+//we need to specify what data to require
+//model should always be uppercase
+const Fruits = require('../models/fruits')
+
+//route (URL you can go to/address)
+//INDEX route-- this will list all the routes
+router.get('/Fruits', (req, res) => {
+  res.render('index.ejs', {
+    fruits: Fruits //<--the data
+  })
+});
+
+//NEW route ---
+//get/fruits/NEW route
+//new template (EJS) --
+router.get('/fruits/new', (req, res) => {
+  res.render('new.ejs', {
+    fruit: Fruits[req.params.id]
+  })
+});
+
+//PUT route// to put edit page up
+router.get('/fruits/:index/edit', (req, res) => {
+  res.render('edit.ejs', {
+    fruit: Fruits[req.params.index],
+    index: req.params.index
+  })
+})
+
+//SHOW route --- show all the info about one partiuclar fruit
+router.get('/fruits/:id', (req, res) => {
+  // const index = req.params.id
+  // res.send(fruits[index]);
+
+  //you "render" templates where you previusly just "send"ed data
+  //you can pass in the data you want to display in your template as the second parameter
+  //your data will ***ALWAYS** be an object
+  res.render('show.ejs', {
+    fruit: Fruits[req.params.id]
+  })
+});
+
+
+//POST route
+//post/fruits
+//no id needed, because this is a new fruit
+router.post('/fruits', (req, res) => {
+  console.log(req.body);
+//add a new object to our fruits array
+//if req.body.readyToEat is on
+  const newFruit = {
+      name: req.body.name,
+      color: req.body.color,
+      readyToEat: req.body.readyToEat == 'on' ? true : false
+  }
+
+  fruits.push(newFruit);
+  res.redirect('/fruits')
+
+});
+
+//DELETE route
+//delete using the index of data in model
+router.delete('/fruits/:id', (req, res) => {
+  fruits.splice(req.params.id, 1);
+  res.redirect('/fruits')
+})
+
+//PUT route
+router.put('/fruits/:id', (req, res) => {
+  console.log(req.body);
+  const theFruit = {
+    name: req.body.name,
+    color: req.body.color,
+    readyToEat: req.body.readyToEat == 'on' ? true : false
+  }
+  Fruits[req.params.id] = theFruit;
+
+  res.send("got it");
+  res.redirect('/fruits');
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override')
 
 //our data
 //note the syntax to put in file, you need relative path--must start w/ "./"
-const fruits = require('./models/fruits.js')
+const fruitController = require('./controllers/FruitController');
 
 //MIDDLEWARE
 //must go above routes you are intercepting
@@ -26,84 +26,6 @@ app.use(methodOverride('_method'))
 //this will let us include static assets (CSS, images)
 app.use(express.static('public'))
 
-
-//route (URL you can go to/address)
-//INDEX route-- this will list all the routes
-app.get('/fruits', (req, res) => {
-  res.render('index.ejs', {
-    fruits: fruits //<--the data
-  })
-});
-
-//NEW route ---
-//get/fruits/NEW route
-//new template (EJS) --
-app.get('/fruits/new', (req, res) => {
-  res.render('new.ejs', {
-    fruit: fruits[req.params.id]
-  })
-});
-
-//PUT route// to put edit page up
-app.get('/fruits/:index/edit', (req, res) => {
-  res.render('edit.ejs', {
-    fruit: fruits[req.params.index],
-    index: req.params.index
-  })
-})
-
-//SHOW route --- show all the info about one partiuclar fruit
-app.get('/fruits/:id', (req, res) => {
-  // const index = req.params.id
-  // res.send(fruits[index]);
-
-  //you "render" templates where you previusly just "send"ed data
-  //you can pass in the data you want to display in your template as the second parameter
-  //your data will ***ALWAYS** be an object
-  res.render('show.ejs', {
-    fruit: fruits[req.params.id]
-  })
-});
-
-
-//POST route
-//post/fruits
-//no id needed, because this is a new fruit
-app.post('/fruits', (req, res) => {
-  console.log(req.body);
-//add a new object to our fruits array
-//if req.body.readyToEat is on
-  const newFruit = {
-      name: req.body.name,
-      color: req.body.color,
-      readyToEat: req.body.readyToEat == 'on' ? true : false
-  }
-
-  fruits.push(newFruit);
-  res.redirect('/fruits')
-
-});
-
-//DELETE route
-//delete using the index of data in model
-app.delete('/fruits/:id', (req, res) => {
-  fruits.splice(req.params.id, 1);
-  res.redirect('/fruits')
-})
-
-//PUT route
-app.put('/fruits/:id', (req, res) => {
-  console.log(req.body);
-  const theFruit = {
-    name: req.body.name,
-    color: req.body.color,
-    readyToEat: req.body.readyToEat == 'on' ? true : false
-  }
-  fruits[req.params.id] = theFruit;
-
-  res.send("got it");
-  res.redirect('/fruits');
-})
 
 app.listen(3000, () => {
   console.log("server is listening on Port 3000");
